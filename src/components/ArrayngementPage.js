049f1118ce17e259d8979c4e46abaf6cf6fa438a
sarahcheatham <sarah.cheatham14@gmail.com>
2019-01-02 18:31:51 -0600

    BOYscore MOYscore EOYscore and EOYgoal sorts all working

--- src/components/ArrayngementPage.js
+++ src/components/ArrayngementPage.js
@@ -51,18 +51,43 @@ class ArrayngementPage extends Component{
             }
         }
         const relevantStudents = students.filter(relevantStudentsCheck);
-        
+        const studentArr = relevantStudents.slice();
         if(this.state.sortBy === "BOY score"){
-            const sortStudentsBoy = relevantStudents.sort((a, b)=>{
+            const sortStudentsBoy = studentArr.sort((a, b)=>{
                 return b.score[0].BOYscore - a.score[0].BOYscore
-            })
+            });
             let student = sortStudentsBoy.map((student, index)=>{
                 return <li key={index}><div className="student">{student.name}</div></li>
             })
-        } 
-        let student = relevantStudents.map((student, index)=>{
+        }
+        if(this.state.sortBy === "MOY score"){
+             const sortStudentsMoy = studentArr.sort((a, b)=>{
+                 return b.score[2].MOYscore - a.score[2].MOYscore
+             })
+             let student = sortStudentsMoy.map((student, index)=>{
+                 return <li key={index}><div className="student">{student.name}</div></li>
+             })
+        }
+        if(this.state.sortBy === "EOY score"){
+            const sortStudentsEoy = studentArr.sort((a, b)=>{
+                return b.score[3].EOYscore - a.score[3].EOYscore
+            })
+            let student = sortStudentsEoy.map((student, index)=>{
+                return <li key={index}><div className="student">{student.name}</div></li>
+            })
+        }
+        if(this.state.sortBy === "EOY goal"){
+            const sortStudentsEoyGoal = studentArr.sort((a, b)=>{
+                return b.score[1].EOYgoal - a.score[1].EOYgoal
+            })
+            let student = sortStudentsEoyGoal.map((student, index)=>{
+                return <li key={index}><div className="student">{student.name}</div></li>
+            })
+        }
+        let student = studentArr.map((student, index)=>{
             return <li key={index}><div className="student">{student.name}</div></li>
         })
+        
         return(
             <div className="arrayngementpage">
                 <span className="inputbar">
