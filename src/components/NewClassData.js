cf3ed4b49d9661b63181fe9222a0631db5a0ed7c
sarahcheatham <sarah.cheatham14@gmail.com>
2019-01-05 16:42:22 -0600

    fixing classdata

---
+++ src/components/NewClassData.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import SubHeader from './SubHeader';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+class NewClassData extends Component{
+    render(){
+        return(
+            <form></form>
+        )
+    }
+}
+
+export default NewClassData;
