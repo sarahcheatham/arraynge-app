322cbbf4cfe1e118b4b88bcb92149443a2b0b6a2
sarahcheatham <sarah.cheatham14@gmail.com>
2019-04-01 18:49:22 -0500

    clearing out old code

--- src/components/ArrayngementPage/ArrayngementPage.js
+++ src/components/ArrayngementPage/ArrayngementPage.js
@@ -27,7 +27,6 @@ class ArrayngementPage extends Component{
         this.handleSubjectChange = this.handleSubjectChange.bind(this);
         this.handleGroupsChange = this.handleGroupsChange.bind(this);
         this.allowDrop = this.allowDrop.bind(this);
-        // this.drag = this.drag.bind(this);
         this.drop = this.drop.bind(this);
         this.sortStudentScore = this.sortStudentScore.bind(this);
     }
@@ -47,62 +46,6 @@ class ArrayngementPage extends Component{
         console.log("this.state.students:", this.state.students)
         console.log("this.state.gradelevel:", this.state.gradelevel)
         console.log("this.state.subject:", this.state.subject)
-
-        //     const filteredStudents = students.filter(relevantStudentsCheck);
-        //     const lastStudent = filteredStudents[filteredStudents.length -1];
-        //     const gradelevel = lastStudent.gradelevel;
-        //     const subject = lastStudent.subject;
-
-
-        // this.props.loadUserId();
-        // fetch(`/api/studentdata`).then((res)=>{
-        //     return res.json()
-        // }).then((students)=>{
-        //     const relevantStudentsCheck = (students) =>{
-        //         if(students !== null){
-        //             return students.userId === this.props.userId
-        //         }
-        //     }
-        //     const filteredStudents = students.filter(relevantStudentsCheck);
-        //     const lastStudent = filteredStudents[filteredStudents.length -1];
-        //     const gradelevel = lastStudent.gradelevel;
-        //     const subject = lastStudent.subject;
-        //     this.setState({
-        //         students: filteredStudents,
-        //         gradelevel: gradelevel,
-        //         subject: subject
-        //     })
-        //     console.log(this.state.students)
-        // }) 
-        // fetch(`/api/studentdata/:id`).then((res)=>{
-        //     return res.json()
-        // }).then((filteredStudents)=>{
-        //     console.log('filteredStudents:', filteredStudents)
-        // }) 
-        
-        // fetch(`/api/studentdata/${this.state.userId}`).then((res)=>{
-        //     return res.json()
-        // }).then((filteredStudents)=>{
-        //     console.log('filteredStudents:', filteredStudents)
-        // })
-        // fetch("/api/studentdata").then((res)=>{
-        //     return res.json()
-        // }).then((students)=>{
-        //     const relevantStudentsCheck = (students) =>{
-        //         if(students !== null){
-        //             return students.userId === this.props.userId
-        //         }
-        //     }
-        //     const relevantStudents = students.filter(relevantStudentsCheck);
-        //     const lastStudent = relevantStudents[relevantStudents.length -1];
-        //     const subject = lastStudent.subject;
-        //     const gradelevel = lastStudent.gradelevel;
-        //     this.setState({
-        //         students: relevantStudents,
-        //         subject: subject,
-        //         gradelevel: gradelevel
-        //     });
-        // });
     }
 
     handleSortBy(event){
@@ -129,10 +72,6 @@ class ArrayngementPage extends Component{
     allowDrop(allowdropevent){
         allowdropevent.preventDefault();
     }
-    
-     // drag(dragevent){
-    //     dragevent.dataTransfer.setData("text", dragevent.target.id)
-    // }
 
     dragStart = (e, index)=>{
         this.draggedItem = e.target.parentNode.id;
@@ -165,6 +104,7 @@ class ArrayngementPage extends Component{
         }
     }
 
+    //sort students by time of year for tests
     sortStudentScore(filteredStudents, sortBy){
         // const propertyName = this.scoreInfo[sortBy].propertyName;
         // const index = this.scoreInfo[sortBy].index;
@@ -188,7 +128,7 @@ class ArrayngementPage extends Component{
         })
         return studentCards
     }
-
+    //assign a color square to each student based on their scores compared to the benchmarks
     getColorForScore(sortBy, student, benchmark){
         if(sortBy === ""){
             return "blankSquare"
@@ -251,12 +191,9 @@ class ArrayngementPage extends Component{
     }
 
     render(){
-        // const benchmark = [];
-        // let boyBenchmark = null;
-        // let moyBenchmark = null;
-        // let eoyBenchmark = null;
         let numberOfGroupsToShow = "";
         //render correct groups component
+        //make into a hash table
         if(this.state.numberOfGroups === ""){
             numberOfGroupsToShow = <FourGroups onDrop={this.drop} onDragOver={this.allowDrop}/>
         }
@@ -274,20 +211,6 @@ class ArrayngementPage extends Component{
             numberOfGroupsToShow = "";
         }
         const benchmarkScore = this.getBenchmarkForScore(this.state.sortBy);
-        // const findBm = benchmarks.find((bm, index)=>{
-        //     const gradelevel = this.state.gradelevel.toUpperCase();
-        //     const subject = this.state.subject;
-        //     if(bm.gradelevel === gradelevel && bm.subject === subject){
-        //        benchmark.push(bm) 
-        //     }
-        // })
-        // if(benchmark[0] !== undefined){
-        //     //rounds the benchmark down to nearest integer based on decimal
-        //     boyBenchmark = Math.floor(benchmark[0].score[0].BOYscore);
-        //     moyBenchmark = Math.floor(benchmark[0].score[1].MOYscore);
-        //     eoyBenchmark = Math.floor(benchmark[0].score[2].EOYscore);
-        // }
-        // const studentArr = this.state.students.slice();
         const studentArr = this.props.studentdata.students;
         console.log("studentArr:", studentArr)
         //filter students by subject
@@ -302,77 +225,6 @@ class ArrayngementPage extends Component{
         if(this.state.sortBy > ""){
             this.sortStudentScore(filteredStudents, this.state.sortBy)  
         }
-
-        // this.sortStudentScore(filteredStudents, this.sortInfo[this.state.sortBy].index, this.sortInfo[this.state.sortBy].propertyName)
-        // if(this.state.sortBy === "BOY score"){
-        //     this.sortStudentScore(filteredStudents, this.state.sortBy)  
-        // this.sortStudentScore(filteredStudents, 0, "BOYscore")
-            // const sortStudentsBoy = filteredStudents.sort((a, b)=>{
-            //     return b.score[0].BOYscore - a.score[0].BOYscore
-            // });
-            // studentCards = sortStudentsBoy.map((student, index)=>{
-            //     let color = "";
-            //     return <li key={index}>
-            //                 <div className="student">
-            //                     <span className={color}/>
-            //                     <span className="studentName">
-            //                         {student.name}
-            //                     </span>
-            //                 </div>
-            //             </li>
-            // })
-        //}
-        // if(this.state.sortBy === "MOY score"){
-        //     this.sortStudentScore(filteredStudents, 2, "MOYscore")
-        //     //  const sortStudentsMoy = filteredStudents.sort((a, b)=>{
-        //     //      return b.score[2].MOYscore - a.score[2].MOYscore
-        //     //  })
-        //     //  studentCards = sortStudentsMoy.map((student, index)=>{
-        //     //      let color = "";
-        //     //      return <li key={index}>
-        //     //                 <div className="student">
-        //     //                     <span className={color}/>
-        //     //                     <span className="studentName">
-        //     //                         {student.name}
-        //     //                     </span>
-        //     //                 </div>
-        //     //             </li>
-        //     //  })
-        // }
-        // if(this.state.sortBy === "EOY score"){
-        //     this.sortStudentScore(filteredStudents, 3, "EOYscore")
-        //     // const sortStudentsEoy = filteredStudents.sort((a, b)=>{
-        //     //     return b.score[3].EOYscore - a.score[3].EOYscore
-        //     // })
-        //     // studentCards = sortStudentsEoy.map((student, index)=>{
-        //     //     let color = "";
-        //     //     return <li key={index}>
-        //     //                 <div className="student">
-        //     //                     <span className={color}/>
-        //     //                     <span className="studentName">
-        //     //                         {student.name}
-        //     //                     </span>
-        //     //                 </div> 
-        //     //             </li>
-        //     // })
-        // }
-        // if(this.state.sortBy === "EOY goal"){
-        //     this.sortStudentScore(filteredStudents, 1, "EOYgoal")
-        //     // const sortStudentsEoyGoal = filteredStudents.sort((a, b)=>{
-        //     //     return b.score[1].EOYgoal - a.score[1].EOYgoal
-        //     // })
-        //     // studentCards = sortStudentsEoyGoal.map((student, index)=>{
-        //     //     let color = "";
-        //     //     return <li key={index}>
-        //     //                 <div className="student">
-        //     //                     <span className={color}/>
-        //     //                     <span className="studentName">
-        //     //                         {student.name}
-        //     //                     </span>
-        //     //                 </div>
-        //     //             </li>
-        //     // })
-        // }
         //assign a square to each student based on their scores compared to the benchmarks
         studentCards = filteredStudents.map((student, index)=>{
             let color = "";
@@ -381,70 +233,6 @@ class ArrayngementPage extends Component{
             // }
             // if(this.state.sortBy > ""){
             color = this.getColorForScore(this.state.sortBy, student, benchmarkScore)
-            // }
-            // this.getColorForScore(this.state.sortBy, student, benchmarkScore)
-            // if(this.state.sortBy === "BOY score"){
-            //     if(student.score[0].BOYscore >= boyBenchmark +5){
-            //         color = "blueSquare"
-            //     } else if(student.score[0].BOYscore >= boyBenchmark){
-            //         color = "greenSquare"
-            //     } else if(student.score[0].BOYscore >= boyBenchmark -5){
-            //         color = "yellowSquare"
-            //     } else if(student.score[0].BOYscore >= boyBenchmark -10){
-            //         color = "orangeSquare"
-            //     } else if(student.score[0].BOYscore < boyBenchmark -10){
-            //         color = "redSquare"
-            //     } else if(student.score[0].BOYscore === null){
-            //         color = "blankSquare"
-            //     } else {
-            //         color = "blankSquare"
-            //     }
-            // }
-            // if(this.state.sortBy === "MOY score"){
-            //     if(student.score[2].MOYscore >= moyBenchmark +5){
-            //         color = "blueSquare"
-            //     } else if(student.score[2].MOYscore >= moyBenchmark){
-            //         color = "greenSquare"
-            //     } else if(student.score[2].MOYscore >= moyBenchmark -5){
-            //         color = "yellowSquare"
-            //     } else if(student.score[2].MOYscore >= moyBenchmark -10){
-            //         color = "orangeSquare"
-            //     } else if(student.score[2].MOYscore < moyBenchmark -10){
-            //         color = "redSquare"
-            //     } else {
-            //         color = "blankSquare"
-            //     }
-            // }
-            // if(this.state.sortBy === "EOY goal"){
-            //     if(student.score[1].EOYgoal >= eoyBenchmark +5){
-            //         color = "blueSquare"
-            //     } else if(student.score[1].EOYgoal >= eoyBenchmark){
-            //         color = "greenSquare"
-            //     } else if(student.score[1].EOYgoal >= eoyBenchmark -5){
-            //         color = "yellowSquare"
-            //     } else if(student.score[1].EOYgoal >= eoyBenchmark -10){
-            //         color = "orangeSquare"
-            //     } else if(student.score[1].EOYgoal < eoyBenchmark -10){
-            //         color = "redSquare"
-            //     } else {
-            //         color = "blankSquare"
-            //     }
-            // }
-            // if(this.state.sortBy === "EOY score"){
-            //     if(student.score[3].EOYscore >= eoyBenchmark +5){
-            //         color = "blueSquare"
-            //     } else if(student.score[3].EOYscore >= eoyBenchmark){
-            //         color = "greenSquare"
-            //     } else if(student.score[3].EOYscore >= eoyBenchmark -5){
-            //         color = "yellowSquare"
-            //     } else if(student.score[3].EOYscore >= eoyBenchmark -10){
-            //         color = "orangeSquare"
-            //     } else if(student.score[3].EOYscore < eoyBenchmark -10){
-            //         color = "redSquare"
-            //     } else {
-            //         color = "blankSquare"
-            //     }
-            // }
             return <li 
                         key={index}
                         id={index}
